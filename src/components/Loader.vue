3639e39178fa5e9d3f0c87dc77eb3ffc0c20b7b9
chahanul <caroneul3014@gmail.com>
2023-08-20 09:32:44 +0900

    feat: loader conponent추가

---
+++ src/components/Loader.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+let observer: IntersectionObserver | null = null
+
+export interface LoaderProps {
+  event: (
+    continueLoad: () => Promise<void>,
+    endLoad: () => void
+  ) => Promise<void>
+}
+
+const props = defineProps<LoaderProps>()
+
+const loader = ref<HTMLElement | null>(null)
+const off = ref(false)
+
+onMounted(async () => {
+  await setObserver()
+})
+
+onUnmounted(() => {
+  disconnect()
+})
+
+async function setObserver() {
+  off.value = false
+  await nextTick()
+  observer = new IntersectionObserver(callback, { threshold: [1] })
+  if (loader.value) observer.observe(loader.value)
+}
+
+const callback = useDebounceFn((entries: IntersectionObserverEntry[]) => {
+  if (off) return
+  entries.forEach(async (entry) => {
+    if (entry.isIntersecting) {
+      disconnect()
+      await props.event(continueLoad, endLoad)
+    }
+  })
+}, 1000)
+
+async function continueLoad() {
+  await setObserver()
+}
+
+function disconnect() {
+  if (observer) {
+    observer.disconnect()
+    observer = null
+  }
+}
+
+function endLoad() {
+  off.value = true
+  disconnect()
+}
+</script>
+
+<template>
+  <div v-if="!off" ref="loader">로딩중</div>
+</template>
